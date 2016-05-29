3cfb3b5a904ba5862036eb0a0963a4e90b002366
Fakhrul <fakhrul945@gmail.com>
2016-05-29 14:56:32 +0600

    add connection and query method

--- core/init.php
+++ core/init.php
@@ -1,11 +1,35 @@
 <?php
 class Shorten{
 
+	public $config = array
+		(
+			'DBNAME' 	=> 'shortener',
+			'USERNAME' 		=> 'root',
+			'PASSWORD' 	=> ''
+		);
+	public $conn;
 
+	public function __construct(){
+		$this->conn = $this->connection();
+	}
 
-
-
-
+	public function connection (){
+		$config = $this->config;
+		try{
+			$conn = new PDO("mysql:host=localhost;dbname=".$config['DBNAME'], 
+							$config['USERNAME'], $config['PASSWORD']);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $conn ;
+		}catch(PDOException $e){
+			return false;
+		}
+	}
+	public function queryAll($query,$bindings,$conn){
+		$conn = $this->conn;
+		$result = $conn->prepare($query);
+		$result->execute($bindings);
+		return $result;
+	}
 
 	
 }
