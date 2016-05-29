319d56c78e0bf121449cb37104aa80dba1cab060
Fakhrul <fakhrul945@gmail.com>
2016-05-29 18:44:42 +0600

    Add Method to Generate and Insert Code

--- core/init.php
+++ core/init.php
@@ -32,6 +32,12 @@ class Shorten{
 			return $result;
 		}		
 	}
+	public function query($query,$bindings,$conn){
+		$conn = $this->conn;
+		$result = $conn->prepare($query);
+		$result->execute($bindings);
+		return $result;			
+	}
 
 	public function redirect($code){
 		$query = $this->queryAll("SELECT * FROM url WHERE code = :code",array('code'=>$code),$this->conn);
@@ -40,6 +46,32 @@ class Shorten{
 		}else{
 			return false;
 		}
+	}
+
+	public function genereateCode($code){
+		return base_convert($code, 10, 30);
+	}
+
+	public function mkurl($url){
+		$url = htmlspecialchars(trim($url));
+		$conn = $this->conn;
+		if(!filter_var($url,FILTER_VALIDATE_URL)){
+			return false;
+		}else{
+			$query = $this->query("SELECT * FROM url WHERE url = :url",array('url'=>$url),$conn);
+			$query = $query->fetchAll();
+			if( $query->rowCount()>0){
+				$code = $query[0]['code'];
+				return $code;
+			}else{
+				$query = $this->query("INSERT INTO url(url,addtime) VALUES(:url,now())",array('url'=>$url),$conn);
+				$id = $conn->lastInsertId();
+				$genCode = $this->genereateCode($id);
+				$query = query("INSERT INTO url(code) VALUES(:code) WHERE id = :id",array('code'=>$genCode,'id'=>$id),$conn);
+				return $genCode;
+			}
+		}
+		
 	}
 
 	
